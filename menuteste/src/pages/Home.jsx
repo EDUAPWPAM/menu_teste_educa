364a1e13c77cea5db9d920dbab03c9cb06e3e146
EDUAPWPAM <eduardo.bento01@etec.sp.gov.br>
2022-07-19 20:04:06 +0000

    save4.5

--- menuteste/src/pages/Home.jsx
+++ menuteste/src/pages/Home.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import './home.css'
+
 
 export default function Home (props){
     return(
-        <div id="home">
+        <div>
             <h1>Olá, esta é a pagina inicial!</h1>
         </div>
     );
